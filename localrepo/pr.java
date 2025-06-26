f6042315823d8abc6ee123fea47a6858d6e04e5d
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-06-26 21:51:35 +0530

    add new program

--- localrepo/pr.java
+++ localrepo/pr.java
@@ -1,17 +1,16 @@
 public class Recursion{
-    public static void printSum(int i,int n,int sum){
-        if(i==n){
-            sum=sum+i;
-            System.out.println(sum);
-            return;
+    public static int calcfactorial(int n){
+        if(n == 0 || n == 1){
+            return 1;
         }
-
-        sum+=i;
-        printSum(i+1,n,sum);
+       int fact_nm1=calcfactorial(n-1);
+       int fact_n=n*fact_mn1;
+       return fact_n;
 
     }
-    public static void main(String args[]){
-        printSum(1,5,0)
-
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = calcfactorial(n);
+        System.out.println("Factorial of " + n + " is: " + ans);
     }
 }
