c112d1b45022fc016c96385ea40c7321bc9d3b6e
Deepanshu292 <deepanshugupta93933@gmail.com>
2025-06-26 21:41:09 +0530

    adding a new program

--- localrepo/pr.java
+++ localrepo/pr.java
@@ -1,20 +1,17 @@
-package localrepo;
-
-public class Recursion {
-    public static void printNumb(int n) {
-        if(n == 0) {
+public class Recursion{
+    public static void printSum(int i,int n,int sum){
+        if(i==n){
+            sum=sum+i;
+            System.out.println(sum);
             return;
         }
-        System.out.println(n);
-        printNumb(n - 1);
+
+        sum+=i;
+        printSum(i+1,n,sum);
+
     }
+    public static void main(String args[]){
+        printSum(1,5,0)
 
-    public static void main(String[] args) {
-        int n = 5;
-        printNumb(n);
     }
-}
-
-
-    
 }
